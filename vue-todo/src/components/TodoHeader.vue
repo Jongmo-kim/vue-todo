deb0a29feaa8f85e56d8f7b8c6cf104b5164d59d
rpa2 <woojin.kim@itmsg.co.kr>
2021-01-27 18:10:02 +0900

    before refactoring

---
+++ vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        <h2>
+            Todo it! 
+        </h2>
+    </header>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+h1{
+    color : #2F3752;
+    font-weight : 900;
+    margin: 2.5rem 0 1.5rem;
+    
+}
+
+</style>
